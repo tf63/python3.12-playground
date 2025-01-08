68cdcc84222b392ed28973d8fb7be84a79d21307
tf63 <74246282+tf63@users.noreply.github.com>
2025-01-08 11:41:07 +0900

    feat: update ruff setting

--- src/sample/main.py
+++ src/sample/main.py
@@ -1,1 +1,39 @@
-print('Hello World')
+import asyncio
+from typing import Literal
+
+a: list[int] | None = [1]
+a = None
+a = 'a'
+
+b: Literal['b1', 'b2', 'b3'] = 'b1'
+b = 'b2'
+
+
+async def say_hello(delay, name, **kwargs):
+    print(f'Hello, {name}')
+    return name
+
+
+async def main():
+    assert True
+    task1 = asyncio.create_task(say_hello(2, 'Alice'))
+    name = await task1
+    print(name)
+    task2 = asyncio.create_task(say_hello(1, 'Bob'))
+    name = await task2
+    print(name)
+
+
+asyncio.run(main())
+
+
+class A:
+    def __init__(self):
+        self.a = 1
+        self._b = 2
+
+
+k = A()
+
+print(k.a)
+print(k._b)
